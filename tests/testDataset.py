37e7df664fd647e2b63f24289b3e802c565e025b
Gan4x4 <gan4x4@gmail.com>
2024-11-15 19:33:21 +0300

    Initial commit

---
+++ tests/testDataset.py
@@ -0,0 +1,23 @@
+import unittest
+from src.dataset import DoubleMaskDataset
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+class DatasetTest(unittest.TestCase):
+    def test_second_mask(self):
+        transforms = A.Compose(
+            [
+                A.CenterCrop(192, 192),
+                ToTensorV2(),
+            ],
+            additional_targets={'mask2': 'mask'}
+        )
+
+        ds = DoubleMaskDataset("tests/data",transform = transforms )
+        image, mask1, mask2, _ = ds[0]
+        self.assertEqual(mask1.shape, mask2.shape)
+
+
+if __name__ == '__main__':
+    unittest.main()
